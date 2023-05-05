8e7bcb80c68933ce7264de33e4cf6c69b34d62e8
bigbea1r <olegpanarin007@mail.ru>
2023-05-05 12:24:59 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container pt-2">
+    <button class ="btn btn-primary" @click="loadResults" >Load results</button>
+
+    <hr>
+
+    <ul class="list-group">
+      <li class="list-group-item" v-for="result of results">
+        <strong> {{ result }} </strong> 
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        results: []
+      }
+    },
+    methods: {
+      loadResults () {
+        this.$http.get('https://rickandmortyapi.com/api/character/info/count')
+        .then(response => {
+          return response.json()
+        })
+        .then(cars => {
+          this.results = results
+          console.log(results)
+        }) 
+      }
+    }
+  }
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+.body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/warapi.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>War API</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+    const name: "warapi.vue"
+  }
+</script>
